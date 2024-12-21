686562d040d693c996e91f02713d27fd33d47eb2
gaurhav27 <gaurhav.fido@gmail.com>
2024-12-21 11:27:51 +0530

    Program to Reverse a String

---
+++ src/com/practice/code/ReverseString.java
@@ -0,0 +1,42 @@
+package com.practice.code;
+
+import java.util.Scanner;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+
+		String reversedArray = "";
+		String reversed = "";
+
+		//??
+		//Didn't declare reversedString but still it worked but it's not working if we are not declaring reversedArray and reversed
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter the String you want to reverse: ");
+		String inputString = scanner.nextLine();
+
+
+		//Using in-built methods
+		StringBuffer stringBuffer = new StringBuffer(inputString);
+		String reversedString = stringBuffer.reverse().toString();
+		System.out.println("Using in-built method, the Reversed String is : " + reversedString);
+
+
+		//Using Array - Without using in-built methods
+		String[] stringArray = inputString.split("");
+
+		for (int i=stringArray.length-1; i>=0; i--) { //Reverse loop
+			reversedArray = reversedArray + stringArray[i];
+		}
+		System.out.println("Without using in-built method, the Reversed String is : " + reversedArray);
+
+		//Without using Array - Without using in-built methods
+		for (int i=inputString.length()-1; i>=0; i--) {
+			reversed = reversed + inputString.charAt(i);
+		}
+		System.out.println("Without using in-built method, the Reversed String is : " + reversed);
+
+		scanner.close();
+	}
+}
